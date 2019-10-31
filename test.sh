e78123ef78b219da6d651d154d92f4ea29704fd9
stelmaszyk <48010443+stelmaszyk@users.noreply.github.com>
2019-10-31 12:39:43 +0100

    x

--- test.sh
+++ test.sh
@@ -8,6 +8,6 @@ read -p "In a message from us, you should get an individual port numer. Rewrite 
 touch rawflight.sh
 echo "#!/bin/bash
 while [ 0 ]; do
-  socat TCP:rawflight.eu:"$(portnumber)" TCP:127.0.0.1:30003
+  socat TCP:rawflight.eu:"$portnumber" TCP:127.0.0.1:30003
   sleep 3;
 done" > rawflight.sh
