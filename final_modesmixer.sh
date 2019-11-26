4d6eb186f312730e1d4b6397acf3c5dfc6db71ff
Bruno Stelmaszyk <48010443+stelmaszyk@users.noreply.github.com>
2019-11-26 12:42:14 +0100

    Final script for ModeSMIxer
    Final

---
+++ final_modesmixer.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+while [ 0 ]; do
+./modesmixer2 \
+--inServer 48581 \
+--outServer msg:33336 \
+--location 52.06759:19.741915 \
+--web 8888 \
+sleep 20;
+#restart
+done;
--- test_modesmixer.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-./modesmixer2 \
---inServer msg:48581 \
---outServer msg:33333 \
---location 52.06759:19.741915 \
---web 8888 \
